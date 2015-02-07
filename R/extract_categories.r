dcd4f3944728351b4fa0beb8887292c2ee635703
Bernhard Pröll <bmutbuerger@gmail.com>
2015-02-07 19:01:08 +0100

    do not export extract_categories_total.

--- R/extract_categories.r
+++ R/extract_categories.r
@@ -1,3 +1,20 @@
+##' Extract the categories of an org file.
+##'
+##' This function is used to extract the categories matching the
+##' regexp pattern.
+##' @param x org object as character vector.
+##' @return the categories of an orgfile as a character vector.
+##' @export extract_categories
+##' @examples
+##' system.file("extdata", "sample.org", package = "orgclockr") %>%
+##' readLines() %>%
+##' extract_categories() %>%
+##' first()
+##' ## [1] "CategoryOne"
+##' @seealso \code{extract_efforts}, \code{extract_todostates},
+##' \code{extract_headlines}, \code{extract_timestamps},
+##' \code{extract_tags} and \code{extract_levels} to extract other org
+##' elements.
 extract_categories <-
     function(x) {
         split_file <- split_orgfile(x)
