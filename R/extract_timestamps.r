2388305071a098bf38589303e5b0df19bcaaaee5
Bernhard Pröll <bmutbuerger@gmail.com>
2015-02-08 11:22:19 +0100

    documentation timestamps. do not export extract_timestamps_total.

--- R/extract_timestamps.r
+++ R/extract_timestamps.r
@@ -1,3 +1,23 @@
+##' Extract the timestamps of an org file per headline.
+##'
+##' This function is used to extract the timestamps per headline
+##' matching a regular expression.
+##' @param x org object as character vector.
+##' @return the timestamps of an orgfile as a list.
+##' @export extract_timestamps
+##' @examples
+##' system.file("extdata", "sample.org", package = "orgclockr") %>%
+##' readLines() %>%
+##' extract_timestamps() %>%
+##' last()
+##' ##  [1] "2015-01-19 Mo 22:57" "2015-01-20 Di 00:05" "2015-01-19 Mo 21:55"
+##' ##  [4] "2015-01-19 Mo 22:12" "2015-01-19 Mo 21:43" "2015-01-19 Mo 21:44"
+##' ##  [7] "2015-01-19 Mo 20:40" "2015-01-19 Mo 21:42" "2015-01-19 Mo 19:25"
+##' ## [10] "2015-01-19 Mo 19:41" "2015-01-19 Mo 16:11" "2015-01-19 Mo 19:01"
+##' @seealso \code{extract_efforts}, \code{extract_todostates},
+##' \code{extract_headlines}, \code{extract_tags},
+##' \code{extract_levels} and \code{extract_categories} to extract
+##' other org elements.
 extract_timestamps <-
     function(x) {
         split_file <- split_orgfile(x)
--- R/extract_timestamps_total.r
+++ R/extract_timestamps_total.r
@@ -1,24 +1,7 @@
 ##' Extract the timestamps of an org file.
 ##'
-##' This function is used to extract the timestamps matching the
-##' regexp pattern.
 ##' @param x org object as character vector.
 ##' @return the timestamps of an orgfile as a list.
-##' @export extract_timestamps_total
-##' @examples
-##' system.file("extdata", "sample.org", package = "orgclockr") %>%
-##' readLines() %>%
-##' extract_timestamps_total()[c(55:56)]
-##' ## [[1]]
-##' ## [1] "2015-01-01 Do 17:40" "2015-01-01 Do 18:14"
-##' ##
-##' ## [[2]]
-##' ## [1] "2014-12-31 Mi 10:51" "2014-12-31 Mi 11:14"
-##' ##
-##' @seealso \code{extract_efforts_total}, \code{extract_todostates},
-##' \code{extract_headlines}, \code{extract_tags},
-##' \code{extract_levels} and \code{extract_categories} to extract
-##' other org elements.
 extract_timestamps_total <-
     function(x) {
         x %>%
